48209a9a9e20b23a9b7210715833a839e5a008eb
Jura <codejura@gmail.com>
2026-03-17 11:13:07 +0800

    js fixed

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-const currentPage = window.location.pathname.split("/").pop();
-
-document.querySelectorAll(".navbar a").forEach(link => {
-    if (link.getAttribute("href") === currentPage) {
-        link.classList.add("active");
-    }
-});
-
-
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
 
